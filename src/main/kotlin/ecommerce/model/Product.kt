2536e0f924b143f412b4931208b0b664c08d5a12
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-07-16 14:08:16 +0200

    feat: add Product model with update and toEntity methods

---
+++ src/main/kotlin/ecommerce/model/Product.kt
@@ -0,0 +1,25 @@
+package ecommerce.model
+
+class Product(
+    var id: Long? = null, var name: String? = null, var price: Double? = null,
+    var imageUrl: String? = null
+) {
+    fun update(newProduct: Product) {
+        newProduct.name?.let { this.name = it }
+        newProduct.price?.let { this.price = it }
+        newProduct.imageUrl?.let { this.imageUrl = it }
+    }
+
+    companion object {
+        fun toEntity(
+            id: Long,
+            product: Product
+        ): Product {
+            return Product(id, product.name, product.price, product.imageUrl)
+        }
+
+    }
+
+    init {
+    }
+}
