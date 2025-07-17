228ff9799d3d937372f988b36ab1fbbf6499bd6f
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-17 16:04:14 +0200

    fix(*.sql): move sql files to right location

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO TBL_PRODUCTS (name, price, imageUrl) VALUES
+('Car', 1000.0, 'www.some.com'),
+('Bike', 200.0, 'www.example.com/bike'),
+('Truck', 30000.0, 'www.example.com/truck'),
+('Laptop', 1500.0, 'www.example.com/laptop'),
+('Phone', 800.0, 'www.example.com/phone');
