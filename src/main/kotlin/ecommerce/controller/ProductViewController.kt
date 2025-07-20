db7b9286d5771b621b55fc8b9871e345ffe6ccf9
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-07-20 15:41:52 +0200

    feat: enhance product management UI with modal for adding new products and update ProductViewController for product creation

--- src/main/kotlin/ecommerce/controller/ProductViewController.kt
+++ src/main/kotlin/ecommerce/controller/ProductViewController.kt
@@ -1,9 +1,11 @@
 package ecommerce.controller
 
+import ecommerce.model.ProductDTO
 import ecommerce.services.ProductService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -13,5 +15,10 @@ class ProductViewController(private val productService: ProductService) {
     fun showProducts(model: Model): String {
         model.addAttribute("products", productService.findAll())
         return "product-list"
+    }
+    @PostMapping("/products")
+    fun createProduct(productDTO: ProductDTO): String {
+        productService.save(productDTO)
+        return "redirect:/"
     }
 }
