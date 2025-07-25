2f450057a2824e6bb4c8eb525f005b252a7370ce
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-07-25 15:22:43 +0200

    test: remove `ProductControllerTest` and clean up unused imports in `ApiErrorControllerAdvice`

--- src/main/kotlin/ecommerce/advice/ApiErrorControllerAdvice.kt
+++ src/main/kotlin/ecommerce/advice/ApiErrorControllerAdvice.kt
@@ -1,10 +1,8 @@
 package ecommerce.advice
 
-
 import ecommerce.exception.AuthorizationException
 import ecommerce.exception.ForbiddenException
 import ecommerce.exception.InvalidCartItemQuantityException
-
 import ecommerce.exception.NotFoundException
 import ecommerce.exception.OperationFailedException
 import ecommerce.util.logger
