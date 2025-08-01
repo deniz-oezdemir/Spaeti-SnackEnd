1dea89caf8f577e8fc40cff9f95d8236480a1b3b
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-08-01 14:59:52 +0200

    feat(option): add `OptionMapper` and `OptionRepository` classes
    - Introduced `OptionMapper` for mapping between `Option` and `OptionDTO` objects.
    - Added `OptionRepository` extending `JpaRepository` for `Option` entity persistence.

---
+++ src/main/kotlin/ecommerce/repositories/OptionRepository.kt
@@ -0,0 +1,7 @@
+package ecommerce.repositories
+
+import ecommerce.entities.Option
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface OptionRepository: JpaRepository<Option, Long> {
+}
