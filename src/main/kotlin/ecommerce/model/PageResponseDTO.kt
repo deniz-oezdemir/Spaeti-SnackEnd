5fdd22046d7522134bdfda4813b69155a80e0459
Deniz Oezdemir <oezdemir.de@gmail.com>
2025-08-04 21:48:26 +0200

    refactor(model): provide specific types for PageResponseDTO - Add specific `PageableDTO` and `SortDTO` data classes to model the structure of Spring's pagination response

--- src/main/kotlin/ecommerce/model/PageResponseDTO.kt
+++ src/main/kotlin/ecommerce/model/PageResponseDTO.kt
@@ -7,9 +7,24 @@ data class PageResponseDTO<T>(
     val size: Int,
     val number: Int,
     val numberOfElements: Int,
-    val sort: Any?,
-    val pageable: Any?,
+    val sort: SortDTO?,
+    val pageable: PageableDTO?,
     val first: Boolean,
     val last: Boolean,
     val empty: Boolean,
+)
+
+data class PageableDTO(
+    val sort: SortDTO,
+    val offset: Long,
+    val pageNumber: Int,
+    val pageSize: Int,
+    val paged: Boolean,
+    val unpaged: Boolean,
+)
+
+data class SortDTO(
+    val empty: Boolean,
+    val sorted: Boolean,
+    val unsorted: Boolean,
 )
