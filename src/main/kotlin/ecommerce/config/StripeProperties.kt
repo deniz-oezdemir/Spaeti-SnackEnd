df830794401e98fba5dc6cfa944f5a066910738c
Deniz Oezdemir <oezdemir.de@gmail.com>
2025-08-12 15:53:35 +0200

    feat(security): add usage of external stripe key

--- src/main/kotlin/ecommerce/config/StripeProperties.kt
+++ src/main/kotlin/ecommerce/config/StripeProperties.kt
@@ -4,5 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("stripe")
 data class StripeProperties(
-    val secretKey: String = "placeholder_key" // TODO: change back to real key or do not use stripe
+    val secretKey: String,
 )
