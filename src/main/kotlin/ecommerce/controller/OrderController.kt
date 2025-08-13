42f03add141d278536e4c731f210d98c050f1758
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-08-13 05:45:32 +0200

    feat: add OrderController for handling order placement

---
+++ src/main/kotlin/ecommerce/controller/OrderController.kt
@@ -0,0 +1,40 @@
+package ecommerce.controller
+
+import ecommerce.annotations.LoginMember
+import ecommerce.dto.MemberResponse
+import ecommerce.dto.PlaceOrderRequest
+import ecommerce.dto.PlaceOrderResponse
+import ecommerce.repository.MemberRepositoryJpa
+import ecommerce.service.OrderService
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+
+@RestController
+@RequestMapping("/orders")
+class OrderController(
+    private val orderService: OrderService,
+    private val memberRepository: MemberRepositoryJpa,
+) {
+    @PostMapping
+    fun placeOrder(
+        @LoginMember member: MemberResponse,
+        @Valid @RequestBody req: PlaceOrderRequest,
+    ): ResponseEntity<PlaceOrderResponse> {
+        val member =
+            memberRepository.findById(member.id)
+
+        if (member.isEmpty) {
+            return ResponseEntity.badRequest().build()
+        } else {
+            val res = orderService.place(member.get(), req)
+            return ResponseEntity
+                .created(URI.create("/orders/${res.orderId}"))
+                .body(res)
+        }
+    }
+}
